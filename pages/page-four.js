7cfdecdb6b243dcebb236ac23b098b7748b5de74
kraaghavin <kraaghav.in@gmail.com>
2022-05-01 09:41:48 +0530

    a

--- pages/page-four.js
+++ pages/page-four.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import Scroller from '../components/scroller';
-import Carousel from '../components/Carousel/Carousel';
+// import React from 'react';
+// import Scroller from '../components/scroller';
+// import Carousel from '../components/Carousel/Carousel';
+// import Image from 'next/image';
 
-export default function Carousels() {
-  return (
-    <Scroller>
-    <div>
-      <Carousel
-        show={1}
-        infiniteLoop
-        withIndicator
-      >
-      <h2 data-testid="carousel-item-1"><img src="https://wallpapercave.com/wp/wp7333471.jpg" alt="placeholder" />asdfasdf</h2>
-      <h2 data-testid="carousel-item-2"><img src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-3"><img src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-1"><img src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-2"><img src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-3"><img src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-1"><img src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-2"><img src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-3"><img src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-1"><img src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-2"><img src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-3"><img src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-1"><img src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-2"><img src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
-      <h2 data-testid="carousel-item-3"><img src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
-      </Carousel>
-    </div>
-    </Scroller>
-  );
-}
+// export default function Carousels() {
+//   return (
+//     <Scroller>
+//     <div>
+//       <Carousel
+//         show={1}
+//         infiniteLoop
+//         withIndicator
+//       >
+//       <h2 data-testid="carousel-item-1"><Image src="https://wallpapercave.com/wp/wp7333471.jpg" alt="placeholder" />asdfasdf</h2>
+//       <h2 data-testid="carousel-item-2"><Image src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-3"><Image src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-1"><Image src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-2"><Image src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-3"><Image src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-1"><Image src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-2"><Image src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-3"><Image src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-1"><Image src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-2"><Image src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-3"><Image src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-1"><Image src="https://wallpapercave.com/wp/wp5296530.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-2"><Image src="https://wallpapercave.com/wp/wp7333475.jpg" alt="placeholder" />asdf</h2>
+//       <h2 data-testid="carousel-item-3"><Image src="https://wallpapercave.com/wp/wp3252821.jpg" alt="placeholder" />asdf</h2>
+//       </Carousel>
+//     </div>
+//     </Scroller>
+//   );
+// }
